5efa50cf4ed856a39dc619452a7e56c098901ac7
sumi04062 <sumi72541@gmail.com>
2024-06-21 00:16:31 +0600

    Initial Commit
    Database connection, Category

---
+++ Expense Pro/Expense Pro/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Expense_Pro.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; } //primary key
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Title { get; set; }  //maybe for naming the expense
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string Icon { get; set; } = "";  //this property is used for icons of category
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string Type { get; set; } = "Expense";  //meaning type is income or expense.
+                                                       //Most of the case it will be expense-type.By default type is "Expense."
+                                                       //So initialized with default type="expense"
+
+    }
+}
---
+++ Expense Pro/Expense Pro/Models/Transaction.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Expense_Pro.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; } //primary key
+
+        //CategoryId
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+
+
+        public int Amount { get; set; } //transaction amount in dollar/taka
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; } //write detail transation note.
+                                          //?is for nullable datatype. 
+
+        public DateTime Date { get; set; } = DateTime.Now; //transation time of a transaction 
+
+    }
+}
